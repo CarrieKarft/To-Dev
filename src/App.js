b0ef53c1e033132e1d25b8cdda6737a989278520
Carrie Kraft <carriekarft@gmail.com>
2023-03-15 18:54:53 -0700

    adding components and rendering them in the App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react"
+import NavBar from './components/NavBar';
+import AllTasksList from './components/AllTaskList';
+import ProjectList from './components/ProjectsList';
+import NewTaskForm from './components/NewTaskForm';
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8000/tasks")
+    .then(r => r.json())
+    .then(data => {
+      // console.log(data)
+      setData(data)
+    })
+  }, [])
   return (
     <div>
-    
+      <NavBar />
+      <AllTasksList data={data}/>
+      <ProjectList />
+      <NewTaskForm />
     </div>
   );
 }
 
 export default App;
+ 
