79d07e94c180140b19568d6deb681ea76b6af793
Carrie Kraft <carriekarft@gmail.com>
2023-03-17 15:51:43 -0700

    filtering over the task data to match tasks to their specific projects and display them on the page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,21 @@
 import React from "react";
 
-function Project() {
+function Project({ project, data }) {
+    // console.log(project)
+    // console.log(data.project)
+    const filteringData = data.filter(task => {
+        return task.project === project
+    })
+    console.log(filteringData)
+
     return (
         <div>
-            <h1>Projects List</h1>
+            <h2>{project}</h2>
+            {/* <h1>Projects List</h1>
             <card>React Project</card>
             <card>JavaScript Project</card>
-            <card>Python Project</card>
+            <card>Python Project</card> */}
+            <ul>{filteringData.map(projectTask => <li key={projectTask.id}>{projectTask.task}</li>)}</ul>
         </div>
      )
 }
