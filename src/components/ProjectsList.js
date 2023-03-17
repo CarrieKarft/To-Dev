3ab59e44c90803368dbf3d2aab71604994e70472
Carrie Kraft <carriekarft@gmail.com>
2023-03-17 15:49:43 -0700

    adding a projects array for now to represent the project categories and mapping over them to render a Project component for each one

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,12 +1,27 @@
 import React from "react";
+import Project from "./Project";
 
-function ProjectList() {
+function ProjectList({ data }) {
+    // console.log(data)
+    const projects = ["React App", "JavaScript App", "Python App"]
+    const mapingprojects = projects.map(project => {
+        return <Project data={data} key={project} project={project}/>
+    })
+    // const filteringData = data.filter(task => {
+    //     if (task.project === task.project){
+    //         return task
+    //     }
+        
+    // })
+    // console.log(filteringData)
+
  return (
     <div>
         <h1>Projects List</h1>
-            <div>React Project</div>
+            {/* <div>React Project</div>
             <div>JavaScript Project</div>
-            <div>Python Project</div>
+            <div>Python Project</div> */}
+            {mapingprojects}
     </div>
  )
 }
