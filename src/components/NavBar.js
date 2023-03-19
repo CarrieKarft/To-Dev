04e7b5ef0bb6650ca885ce7f4a423d6eadb7f603
Carrie Kraft <carriekarft@gmail.com>
2023-03-19 14:23:22 -0700

    adding styling to NavLink components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,20 @@ import React from "react";
 import {NavLink} from "react-router-dom"
 
 function NavBar() {
+    const linkStyles = {
+        width: "50px",
+        padding: "12px",
+        margin: "0 6px 6px",
+        textDecoration: "none",
+        color: "blue",
+      };
+
     return (
         <div>
             <h1>Navigation</h1>
-            <NavLink to="/all-tasks">All Tasks</NavLink>
-            <NavLink to="/projects-list">Projects List</NavLink>
-            <NavLink to="/new-task-form">New Task Form</NavLink>
+            <NavLink to="/all-tasks" style={linkStyles}>All Tasks</NavLink>
+            <NavLink to="/projects-list" style={linkStyles}>Projects List</NavLink>
+            <NavLink to="/new-task-form" style={linkStyles}>New Task Form</NavLink>
         </div>
      )
 }
