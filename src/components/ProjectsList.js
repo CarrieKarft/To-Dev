dc5a758070d0dd9e4552ef9f95d8a00c5ac972f3
Carrie Kraft <carriekarft@gmail.com>
2023-03-26 14:49:35 -0700

    adding state to project list so that user can add new projects

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,27 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import Project from "./Project";
 
-function ProjectList({ data }) {
-    // console.log(data)
-    const projects = ["React App", "JavaScript App", "Python App"]
-    const mapingprojects = projects.map(project => {
+function ProjectList({ data, projectList, setProjectList }) {
+    const [newProjectName, setNewProjectName] = useState('')
+    const mapingprojects = projectList.map(project => {
         return <Project data={data} key={project} project={project}/>
     })
-    // const filteringData = data.filter(task => {
-    //     if (task.project === task.project){
-    //         return task
-    //     }
-        
-    // })
-    // console.log(filteringData)
+
 
  return (
     <div>
         <h1>Projects List</h1>
-            {/* <div>React Project</div>
-            <div>JavaScript Project</div>
-            <div>Python Project</div> */}
-            {mapingprojects}
+        <form onSubmit={(e) => {
+            e.preventDefault()
+            // console.log(newProjectName)
+            // setProjectList([...projectList, newProjectName])
+            // console.log(projectList)
+
+        }}>
+            <label>New Project Name
+                <input 
+                    type="text" 
+                    value={newProjectName}
+                    onChange={e => {
+                        setNewProjectName(e.target.value)
+                        // console.log(e.target.value)
+                    }}
+                />
+            </label>
+            <input 
+                type="submit" 
+            />
+        </form>
+        {mapingprojects}
     </div>
  )
 }
