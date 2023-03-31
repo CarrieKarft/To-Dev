d546e8e6781eb3f3bd54c511fdd116050aaf05a3
Carrie Kraft <carriekarft@gmail.com>
2023-03-31 15:00:45 -0700

    fixing the component so that it uses values passed from App component instead of Project component

--- src/components/ProjectTasks.js
+++ src/components/ProjectTasks.js
@@ -1,11 +1,24 @@
 import React from "react";
+// import {useParams} from "react-router-dom"
 
-function ProjectTasks({ filteredTask, onDeleteTask }) {
-    // console.log(filteredTask)
+function ProjectTasks({ data, projectList }) {
+
+    const id = 1
+    // console.log(projectList)
+    const findingProject = projectList.filter(project => project.id === id).map(project => {
+        console.log(project)
+        return (
+            <div key={project.id}>
+            <h2>{project.newProjectName}</h2>
+            <p>{project.projectDetails}</p>
+            </div>
+        )
+    })
+    // console.log(findingProject)
+    // const matchingTasksToProject = 
     return (
         <div>
-            <p>{filteredTask.task}</p>
-            <button onClick={() => onDeleteTask(filteredTask)}>Completed!</button>
+         {findingProject}
         </div>
     )
 }
