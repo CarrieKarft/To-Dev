902eac133d2e4f5f6158e2656c6c60efbcf5314e
Carrie Kraft <carriekarft@gmail.com>
2023-03-30 17:25:29 -0700

    adding a Delete HTTP request to update the db.json file and filter out deleted tasks to update state

--- src/App.js
+++ src/App.js
@@ -8,31 +8,65 @@ import ProjectList from './components/ProjectsList';
 import NewTaskForm from './components/NewTaskForm';
 
 
+
 function App() {
   const [data, setData] = useState([])
+  const [projectList, setProjectList] = useState([])
+//  "React App", "JavaScript App", "Python App"
 
   useEffect(() => {
     fetch("http://localhost:8000/tasks")
     .then(r => r.json())
     .then(data => {
+      // data.reverse()
       // console.log(data)
       setData(data)
     })
   }, [])
+
+  useEffect(() => {
+    fetch("http://localhost:8000/projects")
+    .then(r => r.json())
+    .then(data => {
+      // console.log(data)
+      setProjectList(data)
+    })
+  }, [])
+  // console.log(projectList)
+
+  function handleTaskDelete(taskData) {
+    fetch(`http://localhost:8000/tasks/${taskData.id}`,
+    {method: 'DELETE'})
+    .then(r => r.json())
+    .then(() => {
+      filteringDeletedItems(taskData)
+    })
+  }
+
+  function filteringDeletedItems(taskData) {
+    console.log(taskData)
+    const updatingTasks = data.filter(task => {
+      return taskData.id !== task.id
+    })
+    setData(() => updatingTasks)
+  }
+
+  
+
   return (
-    <div>
+    <div className='app'>
       <Route>
           <NavBar exact path="/"/>
       </Route>
       <Switch>
         <Route path="/all-tasks">
-          <AllTasksList data={data}/>
+          <AllTasksList data={data} onDeleteTask={handleTaskDelete}/>
         </Route>
         <Route path="/projects-list">
-          <ProjectList />
+          <ProjectList data={data} projectList={projectList} setProjectList={setProjectList} onDeleteTask={handleTaskDelete}/>
         </Route>
         <Route path="/new-task-form">
-          <NewTaskForm />
+          <NewTaskForm setData={setData} data={data} projectList={projectList}/>
         </Route>
       </Switch>
     </div>
