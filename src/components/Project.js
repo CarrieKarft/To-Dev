50da01b2110a772f267e05ce48764adb741dd561
Carrie Kraft <carriekarft@gmail.com>
2023-03-31 19:43:49 -0700

    deleting filter through project list since it was not needed with fetch request in ProjectTasks component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,34 +1,16 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
-import ProjectTasks from "./ProjectTasks";
+import { Link } from "react-router-dom";
 
 
-function Project({ project, data, onDeleteTask }) {
-    // console.log("project:", project)
-    // console.log("data:", data)
-    // const filteringData = data.filter(task => {
-    //     return task.project === project
-    // })
-    // console.log(filteringData)
-        // console.log(project)
-        const filteringAndMappingData = data.filter(task => {
-            return task.project === project
-        }).map(filteredTask => {
-            // console.log(filteredTask)
-            return (
-                <Route>
-                    <ProjectTasks exact path="/projects-list/tasks" key={filteredTask} filteredTask={filteredTask} onDeleteTask={onDeleteTask}/>
-                </Route>
-            )
-        })
-        // console.log(filteringAndMappingData)
+function Project({ project, details, id }) {
         
 
     return (
         <div className="project">
             <h2>{project}</h2>
-            <Link to="/projects-list/tasks">Click To View Tasks</Link>
-            {filteringAndMappingData}
+            <p>{details}</p>
+            <Link to={`/projects-list/${id}`}>Click To View Tasks</Link>
+            {/* {filteringAndMappingData} */}
             {/* <ul>{filteringData.map(projectTask => <li key={projectTask.id}>{projectTask.task}</li>)}</ul> */}
         </div>
      )
