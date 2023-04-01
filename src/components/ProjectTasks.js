883e387726e131aa46db6dc8ee97def001473884
Carrie Kraft <carriekarft@gmail.com>
2023-03-31 19:40:44 -0700

    adding a fetch for the project information and adding in useParams hook

--- src/components/ProjectTasks.js
+++ src/components/ProjectTasks.js
@@ -1,24 +1,47 @@
-import React from "react";
-// import {useParams} from "react-router-dom"
+import React, {useState, useEffect} from "react";
+import { useParams } from "react-router-dom";
 
-function ProjectTasks({ data, projectList }) {
+function ProjectTasks({ data, onDeleteTask }) {
+    const [projectMatch, setProjectMatch] = useState(null);
 
-    const id = 1
-    // console.log(projectList)
-    const findingProject = projectList.filter(project => project.id === id).map(project => {
-        console.log(project)
-        return (
-            <div key={project.id}>
-            <h2>{project.newProjectName}</h2>
-            <p>{project.projectDetails}</p>
-            </div>
-        )
+    const { id } = useParams()
+    // console.log(params)
+
+    // const id = 3
+    useEffect(() => {
+        fetch(`http://localhost:8000/projects/${id}`)
+        .then(r => r.json())
+        .then(data => setProjectMatch(data))
+    },[id])
+
+    if (!projectMatch) return <h2>Loading...</h2>
+    // console.log(projectMatch)
+    const { projectName, projectDetails } = projectMatch
+    console.log(projectName)
+    console.log(projectDetails)
+    const taskMap = data.map(task => {
+        if (task.project === projectName) {
+            return (
+                <div key={task.id} className="task">
+                    <h4>{task.task}</h4>
+                    <p>{task.completeBy}</p>
+                    <button onClick={() => onDeleteTask(task)}>Completed!</button>
+                </div>
+            )
+        }
+        return null
     })
-    // console.log(findingProject)
-    // const matchingTasksToProject = 
+
+    // console.log(projectMatch.newProjectName)
+ 
+ 
     return (
-        <div>
-         {findingProject}
+        <div className="taskProject">
+            <h1>{projectName}</h1>
+            <h3>{projectDetails}</h3>
+            <h2>{projectName} Tasks</h2>
+            <ul>{taskMap}</ul>
+       
         </div>
     )
 }
